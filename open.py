2ec5042ed4d5665c71eda47f0ba3b435af561938
Samarth H Chinivar <samarthharish321@gmail.com>
2023-03-12 19:53:20 +0530

    first commit

---
+++ open.py
@@ -0,0 +1,10 @@
+# Name: Samarth H Chinivar
+# Company: RedTron Education Service LLP.
+# Position: Python Data Science Intern
+
+# Title: Content Generator
+
+import webbrowser
+
+url = "https://colab.research.google.com/drive/1dwMinx78v5y3fajUPH9nAnOdHBPFZLjN"
+webbrowser.open_new_tab(url)
